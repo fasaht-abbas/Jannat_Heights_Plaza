f1b95fe12029838d4aa2b3854ccd484f49ad77fa
Fasahat Abbas <fasahatabbas3@gmail.com>
2024-07-04 23:17:04 +0500

    add static paths added

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -8,8 +8,23 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const validate_1 = require("./utils/validate");
 const mongoose_1 = __importDefault(require("mongoose"));
 const colors_1 = __importDefault(require("colors"));
+const path_1 = __importDefault(require("path"));
+const express_1 = __importDefault(require("express"));
 // Load environment variables
 dotenv_1.default.config();
+app_1.default.use(express_1.default.static(path_1.default.join(__dirname, "build"), {
+    setHeaders: (res, path) => {
+        if (path.endsWith(".css")) {
+            res.setHeader("Content-Type", "text/css");
+        }
+        else if (path.endsWith(".js")) {
+            res.setHeader("Content-Type", "application/javascript");
+        }
+    },
+}));
+app_1.default.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
+});
 // Setting up REST
 // Final deployment se pehlay ye colors wali sab chezain khatam kar dena.
 mongoose_1.default
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,27 @@ import dotenv from "dotenv";
 import { env } from "./utils/validate";
 import mongoose from "mongoose";
 import colors from "colors";
+import path from "path";
+import express from "express";
 
 // Load environment variables
 dotenv.config();
 
+app.use(
+  express.static(path.join(__dirname, "build"), {
+    setHeaders: (res, path) => {
+      if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      } else if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      }
+    },
+  })
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // Setting up REST
 // Final deployment se pehlay ye colors wali sab chezain khatam kar dena.
 
