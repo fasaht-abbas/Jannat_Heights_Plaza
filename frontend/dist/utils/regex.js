d61dccbb54b79497957aedb0ba1f1c0ddec9ceed
Fasahat Abbas <fasahatabbas3@gmail.com>
2024-07-04 13:50:02 +0500

    build made dist deleted

--- frontend/dist/utils/regex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CNICRegex = exports.fileRegex = exports.phoneRegex = exports.nameRegex = exports.passRegex = exports.emailRegex = void 0;
-exports.emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-exports.passRegex = /^(?=.*[a-zA-Z])(?=.*\d).*$/;
-exports.nameRegex = /^[a-zA-Z\s]+$/;
-exports.phoneRegex = /^\d{8,}$/;
-exports.fileRegex = /(?:\.([^.]+))?$/;
-exports.CNICRegex = /^[0-9]{5}-[0-9]{7}-[0-9]$/;
