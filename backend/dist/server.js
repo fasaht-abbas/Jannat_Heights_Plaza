7b4249c5165e28e11f5a60d8c8fc3a24c94ba61a
Fasahat Abbas <fasahatabbas3@gmail.com>
2024-08-22 12:25:12 +0500

    brought back to http

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -10,15 +10,9 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const colors_1 = __importDefault(require("colors"));
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
-const https_1 = __importDefault(require("https"));
-const fs_1 = __importDefault(require("fs"));
+const http_1 = require("http");
 // Load environment variables
 dotenv_1.default.config();
-const sslOptions = {
-    key: fs_1.default.readFileSync(`/etc/letsencrypt/live/api.jannatheightsplaza.live/privkey.pem`),
-    cert: fs_1.default.readFileSync("/etc/letsencrypt/live/api.jannatheightsplaza.live/fullchain.pem"),
-    ca: fs_1.default.readFileSync("/etc/letsencrypt/live/api.jannatheightsplaza.live/chain.pem"), // Optional, includes the chain of trust
-};
 app_1.default.use(express_1.default.static(path_1.default.join(__dirname, "build"), {
     setHeaders: (res, path) => {
         if (path.endsWith(".css")) {
@@ -32,7 +26,27 @@ app_1.default.use(express_1.default.static(path_1.default.join(__dirname, "build
 app_1.default.get("/*", (req, res) => {
     res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
 });
-const server = https_1.default.createServer(sslOptions, app_1.default);
+// Setting up REST
+// Final deployment se pehlay ye colors wali sab chezain khatam kar dena.
+const server = (0, http_1.createServer)(app_1.default);
+// all the  console logs are commented
+// const io = new Server(server, {
+//   cors: {
+//     origin: env.FRONTEND_URL, // Adjust for your frontend URL
+//     methods: ["GET", "POST"],
+//   },
+// });
+// io.on("connect", (socket) => {
+//   // console.log("A user connected" + socket.id);
+//   socket.on("disconnect", async () => {
+//     // console.log("a user disconnected", socket.id);
+//   });
+// });
+// // Listen for incoming socket connections
+// // Example: Emit an event when a booking is made
+// const notifyAdminBooking = (bookingData: any) => {
+//   io.emit("newBooking", bookingData);
+// };
 mongoose_1.default
     .connect(validate_1.env.MONGO_URL)
     .then(() => {
